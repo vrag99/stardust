cda65852d41ed4431d7f33a0f2b93930d00a9f88
Garv Makkar <garv.codes@gmail.com>
2024-06-22 20:06:21 +0530

    add calculations doc

--- calculations/calculation.js
+++ calculations/calculation.js
@@ -1,17 +1,17 @@
 // these both will come to use from the contracts 
-let value ;
+let value;
 let avg_contribution;
 let k; // constant for proper valuation
 
 function calculate() {
-    let act_val = 1/value;
-    let avg_contribution=1/avg_contribution;
+    let act_val = 1 / value;
+    let avg_contribution = 1 / avg_contribution;
     let x = act_val - avg_contribution;
     let e = Math.E;
-    let collateralizatin_val = 150 * (1 + (k*x) + (k*x)**2/2) / (e**(kx))
+    let collateralizatin_val = 150 * (e ** (kx) / 1 + (k * x) + (k * x) ** 2 / 2)
     // this formula is derived fomr the poisson approximation of the contributions of users
     console.log(collateralizatin_val);// will give us the current values
-    
+
 
 
 }
